c3ccbc9919830046f0a60eaf52969403f21201e9
rodrigoelvira <rodrigof.elvira@gmail.com>
2021-12-15 23:45:45 -0300

    formulario medico creado no tocar

--- AppMedica/urls.py
+++ AppMedica/urls.py
@@ -4,12 +4,12 @@ from AppMedica import views
 
 urlpatterns = [
     
-    path('inicio', views.inicio, name='Inicio'),
-    path('servicios', views.servicios, name='Servicios'),
-    path('pacientes', views.pacientes, name='Pacientes'),
-    path('medicos', views.medicos, name='Medicos'),
-    path('contacto', views.contacto, name='Contacto'),
-    path('medicoformulario',views.medicoFormulario,name="MedicoFormulario"),
+    path('inicio/', views.inicio, name='Inicio'),
+    path('servicios/', views.servicios, name='Servicios'),
+    path('pacientes/', views.pacientes, name='Pacientes'),
+    path('medicos/', views.medicos, name='Medicos'),
+    path('contacto/', views.contacto, name='Contacto'),
+    path('medicoFormulario/',views.medicoFormulario,name="MedicoFormulario"),
     
     
     

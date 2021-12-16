64efafbdeb374c91749574a18bb7374327fd4299
Sasha Daszko <sasha.daszko@gmail.com>
2021-12-15 21:20:11 -0300

    Agregue views, modelos y urls a AppMedica

--- AppMedica/urls.py
+++ AppMedica/urls.py
@@ -4,5 +4,9 @@ from AppMedica import views
 urlpatterns = [
     
     path('inicio', views.inicio, name='Inicio'),
+    path('servicios', views.servicios, name='Servicios'),
+    path('pacientes', views.pacientes, name='Pacientes'),
+    path('medicos', views.medicos, name='Medicos'),
+    path('contacto', views.contacto, name='Contacto'),
 
     ]
--- AppMedica/views.py
+++ AppMedica/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    
+
     return render(request,'AppMedica/inicio.html')
 
 
+
+def servicios(request):
+
+    return render(request,'AppMedica/servicios.html')
+
+
+def pacientes(request):
+
+    return render(request,'AppMedica/pacientes.html')
+
+
+def medicos(request):
+
+    return render(request,'AppMedica/medicos.html')
+
+
+
+def contacto(request):
+
+    return render(request,'AppMedica/contacto.html')
